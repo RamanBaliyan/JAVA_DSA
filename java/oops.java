5211cb7dd61982c23e34ad2e83b88cae04e2271f
RamanBaliyan <chramanbaliyan@gmail.com>
2023-02-27 16:10:19 +0530

    oops_1

---
+++ java/oops.java
@@ -0,0 +1,24 @@
+class pen{
+    int a,b,c;
+     
+    public void sum()
+    {
+c=a+b;
+System.out.println(c);
+    }
+}
+public class oops{
+
+    public static void main(String[] args) {
+      
+        System.out.println("abc");
+     pen a1=new pen();
+     a1.a=10;
+     a1.b=20;
+     a1.sum();
+
+
+
+        
+    }
+}
