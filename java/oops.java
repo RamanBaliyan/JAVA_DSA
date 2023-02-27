a6180f4ba6491745c885bd2c687765c740b4d7f0
RamanBaliyan <chramanbaliyan@gmail.com>
2023-02-27 22:57:55 +0530

    constructor

--- java/oops.java
+++ java/oops.java
@@ -11,15 +11,23 @@ System.out.println(c);
     {
         System.out.println(this.a);
     }
+    pen(pen a2){
+        this.a=a2.a;
+        this.b=a2.b;
+
+    }
+    pen(){}
 }
 public class oops{
 
     public static void main(String[] args) {
      pen a1=new pen();
+
      a1.a=20;
      a1.b=40;
-     a1.printcolor();
-     a1.sum();
+     pen a2=new pen(a1);
+     a2.printcolor();
+     a2.sum();
 
 
 
