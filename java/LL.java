1949933afa1de5053e321e187ba1a8f968d76653
RamanBaliyan <chramanbaliyan@gmail.com>
2023-03-02 20:15:22 +0530

    update

--- java/LL.java
+++ java/LL.java
@@ -1,4 +1,6 @@
 public class LL {
+    int size;
+    node head;
     class node{
         String data;
         node next;
@@ -6,6 +8,7 @@ public class LL {
     node(String data){
         this.data=data;
         this.next=null;
+        size++;
     }
     }
     public void addFirst(String data){
@@ -20,8 +23,77 @@ public class LL {
         head = newnode;
         
     }
+    public void addlast(String data){
+    node newnode = new node(data);
+    if(head == null)
+        {
+            head = newnode;
+            return;
+
+        }
+    node currnode=head; 
+    while(currnode.next!=null){
+        currnode = currnode.next;}
+
+    currnode.next=newnode; 
+      
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("list is empty");
+            return;
+        }
+    
+        node currnode = head; 
+        while(currnode!=null){
+        System.out.print(currnode.data + "->");
+        currnode = currnode.next;
+        } 
+        System.out.println("NULL");
+    }
+    public void firstDelete(){
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        head=head.next;
+        size--;
+    }
+    public void lastDelete(){
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        size--;
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        node secondlast=head;
+        node last=head.next;//head.next=null-.> to null.next error dega
+        while(last.next!=null){
+            last=last.next;
+            secondlast=secondlast.next;
+        }
+        secondlast.next=null;
+    }
+    public void getsize(){
+        System.out.println(size);
+    }
     public static void main(String[] args) {
         LL list=new LL();
+        list.addFirst("a");
+        list.addlast("b");
+        list.print();
+        list.addFirst("c");
+        list.print();
+        list.addlast("Raman");
+        list.print();
+        list.firstDelete();
+        list.lastDelete();
+        list.print();
+        list.getsize();
+        list.addlast("Raman");
+        list.print();
+        list.getsize();
     }
     
 }
