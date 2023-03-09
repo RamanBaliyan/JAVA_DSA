c39f0f684468b5efade7614cbf8aea74effef047
RamanBaliyan <chramanbaliyan@gmail.com>
2023-03-09 14:57:32 +0530

    bst

--- java/BST.java
+++ java/BST.java
@@ -1,3 +1,5 @@
+
+import java.util.*; 
 public class BST {
     
     static class node{
@@ -46,7 +48,84 @@ public class BST {
                     else{
                         search(root.right,element);
                     }
+          
         }
+        public static node delete(node root,int val){
+            if(root.data>val){
+                root.left=delete(root.left,val);
+            }
+            else if(root.data<val){
+               root.right =  delete(root.right,val);
+            }
+            else{
+                if(root.left==null&&root.right==null){
+                    return null;
+                }
+                 if(root.left==null){
+                    return root.right;
+                }
+                else if(root.right==null){
+                    return root.left;
+                }
+                node is= inorderSuccesor(root.right);
+                root.data = is.data;
+                root.right = delete(root.right,is.data);
+                
+            }
+            return root;
+        }
+            public static node inorderSuccesor(node root){
+                while(root.left!=null){
+                    root=root.left;
+                    
+                }
+                return root;
+
+            }
+            public static void range(node root,int x,int y){
+                if(root==null){
+                    return;
+                }
+                if(root.data>=x && root.data<=y){
+                    range(root.left,x,y);
+                    System.out.println(root.data + "");
+                range(root.right,x,y);              
+             }
+             else if(root.data>=y){
+                range(root.left,x,y);
+            
+             }
+             else {
+                range(root.right,x,y);
+             }
+            }
+            public static void PrintPath(ArrayList<Integer>p){
+                for(int i=0;i<p.size();i++){
+                    System.out.print(p.get(i)+ "=>");
+                }
+                System.out.println();
+            }
+            public static void path(node root,ArrayList<Integer> p){
+                if(root==null){
+                    return;
+                }
+                p.add(root.data);
+
+                //leaf
+                if(root.left==null&&root.right==null){
+                    PrintPath(p);
+                }
+                else {
+
+                
+                path(root.left,p);
+                path(root.right,p);
+                
+                }
+                p.remove(p.size()-1);
+            }
+
+        
         
         
     
@@ -58,8 +137,13 @@ public class BST {
       }
       inorder(root);
       System.out.println();
-      search(root,9);
+      // search(root,9);
+      // delete(root,4);
+      // inorder(root);
+      System.out.println();
+     // range(root,2,12);
+      path(root,new ArrayList<>());
     }
 }
-    
+     
 
